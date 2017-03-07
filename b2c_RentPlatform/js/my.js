42b62347d040fa476a9eca70f0b0fb99c3513b38
css <shusen2013@outlook.com>
2017-03-07 18:39:51 +0800

    将app前台加入git
    Signed-off-by: css <shusen2013@outlook.com>

---
+++ b2c_RentPlatform/js/my.js
@@ -0,0 +1,17 @@
+var PHONE_IP = 'http://192.168.201.64:8080/ssm';
+
+var add = function(a, b) {
+	alert(this) //this被绑顶到window
+	return a + b;
+}
+
+// 获取url中的参数
+function getUrlParam(name) {
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+	var r = window.location.search.substr(1).match(reg);
+	if(r != null) {
+		return unescape(r[2]);
+	} else {
+		return null;
+	}
+}
