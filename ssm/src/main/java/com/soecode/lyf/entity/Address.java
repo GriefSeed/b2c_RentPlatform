c585fdfddd75f85ff03e98e7a5e46c99ea62c62a
css <shusen2013@outlook.com>
2017-05-06 12:33:41 +0800

    解决了地址的姓名和电话号码设计问题
    原来的地址设计没有预料到收货人和电话号码这个问题，发现修改要修改很多东西，无奈下只能在购物车页面直接加空格，在后台修改数据库，加长header表里的address长度，因为header表里的address是直接录入字符串，所以就直接将电话和联系人都直接塞进address里了

--- ssm/src/main/java/com/soecode/lyf/entity/Address.java
+++ ssm/src/main/java/com/soecode/lyf/entity/Address.java
@@ -3,32 +3,48 @@ package com.soecode.lyf.entity;
 public class Address {
 	private int addressId;
 	private int accountId;
+	private String name;
+	private String number;
 	private String addressDetail;
-	private int status;
+
 	public int getAddressId() {
 		return addressId;
 	}
+
 	public void setAddressId(int addressId) {
 		this.addressId = addressId;
 	}
+
 	public int getAccountId() {
 		return accountId;
 	}
+
 	public void setAccountId(int accountId) {
 		this.accountId = accountId;
 	}
+
 	public String getAddressDetail() {
 		return addressDetail;
 	}
+
 	public void setAddressDetail(String addressDetail) {
 		this.addressDetail = addressDetail;
 	}
-	public int getStatus() {
-		return status;
+
+	public String getName() {
+		return name;
 	}
-	public void setStatus(int status) {
-		this.status = status;
+
+	public void setName(String name) {
+		this.name = name;
 	}
-	
-	
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
 }
